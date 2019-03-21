8cd0c0fb45e0274fe9df5c21603771ddad44a8c8
Thomas Galliker <thomas@bluewin.ch>
2019-03-21 11:25:17 +0100

    Add conditional compilation example

---
+++ PlatformDivergenceApp/PlatformDivergenceApp.Shared/DatabaseAccess.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.IO;
+
+namespace PlatformDivergenceApp.Services
+{
+    public static class DatabaseAccess
+    {
+        public static string GetDatabaseFilePath()
+        {
+            const string filename = "TodoDatabase.db3";
+            var libraryPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+
+#if __ANDROID__
+            libraryPath = Path.Combine(libraryPath, "Android");
+#elif __IOS__
+            libraryPath = Path.Combine(libraryPath, "..", "Library");
+#endif
+            var path = Path.Combine(libraryPath, filename);
+            return path;
+        }
+    }
+}
