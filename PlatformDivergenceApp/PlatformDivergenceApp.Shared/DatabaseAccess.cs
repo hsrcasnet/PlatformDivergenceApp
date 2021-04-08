fda332b6eeec66d2a01a9880e60d4ca065fb67ae
Thomas Galliker <thomas@bluewin.ch>
2021-04-08 18:52:43 +0200

    Add comments

--- PlatformDivergenceApp/PlatformDivergenceApp.Shared/DatabaseAccess.cs
+++ PlatformDivergenceApp/PlatformDivergenceApp.Shared/DatabaseAccess.cs
@@ -12,8 +12,10 @@ namespace PlatformDivergenceApp.Services
             var libraryPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
 
 #if __ANDROID__
+            // Demo: This code is only compiled on Android:
             libraryPath = Path.Combine(libraryPath, "Android");
 #elif __IOS__
+            // Demo: This code is only compiled on iOS:
             libraryPath = Path.Combine(libraryPath, "..", "Library");
 #endif
             var path = Path.Combine(libraryPath, filename);
